76131bc8d1ec5f4e4deb73a68cd93720e24997b7
RaphaPab <rm98525@fiap.com.br>
2023-09-05 19:44:46 -0300

    Ajustes finais

--- CP2.exerc/src/main.jsx
+++ CP2.exerc/src/main.jsx
@@ -3,45 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
-
-
-
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './routes/Home/index.jsx'
 import Produtos from './routes/Produtos/index.jsx'
 import Error from './routes/Error/index.jsx'
 import EditarProduto from './routes/EditarProdutos/index.jsx'
 
+
+
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-    errorElement: <Error/>,
+  {path: "/" , element: <App/>, errorElement: <Error/>,
     children:[
-      {
-        path:"/",
-        element:<Home/>
-      },
-      {
-        path:"/produtos",
-      element:<Produtos/>
-
-      },
-      {
-        path:"/produtos/editar/:id",
-        element:<EditarProduto/>
-
-      }
-    ]
-  }
+      {path: "/" , element: <Home/>},
+      {path: "/produtos" , element: <Produtos/>},
+      {path: "/editar/produtos/:id" , element: <EditarProduto/>},
+    ]}
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
